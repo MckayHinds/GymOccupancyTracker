54bc529360615107eab6ffde7c00c3583a109925
Mckay Hinds <mhinds03@byui.edu>
2026-01-14 13:53:54 -0700

    Where did all this crap come from

--- GymCounterWeb/obj/Debug/net8.0/GymCounterWeb.AssemblyInfo.cs
+++ GymCounterWeb/obj/Debug/net8.0/GymCounterWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymCounterWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649d519e36abecd884e5cab0966646022a688c8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30d8de554442901a4e1fe9279282f98cb957eec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymCounterWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymCounterWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
