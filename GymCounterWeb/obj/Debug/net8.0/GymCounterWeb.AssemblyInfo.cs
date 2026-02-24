34dc67fe9c7e4974d7aee5877a072a546dcab1e2
Mckay Hinds <mhinds03@byui.edu>
2026-02-23 17:44:36 -0700

    auto tests

--- GymCounterWeb/obj/Debug/net8.0/GymCounterWeb.AssemblyInfo.cs
+++ GymCounterWeb/obj/Debug/net8.0/GymCounterWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymCounterWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f12822818b657216220d04a3e4ebe2a2eba04ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9dca7310a03ac79c76eff9c1c2f19ed8185c8fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymCounterWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymCounterWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
